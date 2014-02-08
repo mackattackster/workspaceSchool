3e26f2d78b59c187bbc57591267bddca9196904c
mackattackster <mackattackster@gmail.com>
2014-02-08 13:41:26 -0500

    Added Assignment 2

---
+++ Assignment2/ch15/ch15/exercises/Exercise15_1.java
@@ -0,0 +1,11 @@
+package ch15.exercises;
+
+/*
+Modify the GeometricObject class to implement the Comparable interface, and define a static max 
+method in the GeometricObject class for finding the larger of two GeometricObject objects. Implement 
+the new GeometricObject class. Write a test program that uses the max method to find the largest of two 
+circles and the larger of two rectangles
+*/
+public class Exercise15_1 {
+
+}
---
+++ Assignment2/ch15/ch15/exercises/Exercise15_2.java
@@ -0,0 +1,13 @@
+package ch15.exercises;
+
+/*
+Design and Interface names Colorable with a void method named howToColor(). Design a class named 
+Square that extends class GeometricObject and implements interface Colorable. Implement method 
+howToColor() to display the message “Color all four sides”. Write a test program that creates an array of 
+six GeometricObject objects (a mix of circles, rectangles, and squares), and for each object in the array 
+invoke its howToColor() method if it is colorable.
+ */
+
+public class Exercise15_2 {
+
+}
